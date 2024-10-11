2c89a14ab0029dd3f37dddc721eb64f383d95c54
Bohurupi Shopping <168202012+bohurupishopping@users.noreply.github.com>
2024-10-11 23:58:34 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white dark:bg-gray-900 shadow-md p-4 text-center transition-colors duration-200">
+    <footer className="bg-white dark:bg-gray-900 shadow-md p-4 pb-10 text-center transition-colors duration-200">
       <p className="text-gray-600 dark:text-gray-400">
         © Bohurupi AppCentral | by Pritam ❤️
       </p>
